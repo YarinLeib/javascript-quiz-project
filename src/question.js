ff7cab71d912baef0c73867d2dc8edc119a035c5
Yarin <yarinleib@gmail.com>
2025-02-06 20:10:23 +0100

    update 1
    Co-authored-by: angeleVG <angeleVG@users.noreply.github.com>

--- src/question.js
+++ src/question.js
@@ -1,7 +1,14 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text; //should receive string
+    this.choices = choices; // should receive array of strings
+    this.answer = answer; // should receice stirng
+    this.difficulty = difficulty; // should receive number
+  }
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+    }
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,40 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions; // should receive array
+    this.timeLimit = timeLimit; // should receive mumber
+    this.timeRemaining = timeRemaining; //should receive number
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (answer === this.getQuestion().answer) {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      this.currentQuestionIndex === this.questions;
+      return true;
+    }
+  }
 }
